a42617a97a77147eb56ae14eb2b33c0321894f11
Andrea Fernandez de la Rosa <a28707@svalero.com>
2025-05-20 17:26:02 +0200

    Creo la clase Usuarios y añado Lombok

--- src/main/java/com/svalero/peliculas/model/Usuarios.java
+++ src/main/java/com/svalero/peliculas/model/Usuarios.java
@@ -1,4 +1,71 @@
 package com.svalero.peliculas.model;
 
+import lombok.Data;
+import java.sql.Date;
+
+@Data
 public class Usuarios {
+    private String idUsuario;
+    private String nombre;
+    private String email;
+    private String contraseña;
+    private String rol;
+    private Date fechaRegistro;
+    private boolean activo;
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
