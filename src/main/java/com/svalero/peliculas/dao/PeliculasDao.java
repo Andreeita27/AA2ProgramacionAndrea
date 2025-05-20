3dc6eabc6b8aaa967c6d89bf60a8399e2b3e4514
Andrea Fernandez de la Rosa <a28707@svalero.com>
2025-05-20 19:17:59 +0200

    Empiezo a generar el DAO de peliculas

--- src/main/java/com/svalero/peliculas/dao/PeliculasDao.java
+++ src/main/java/com/svalero/peliculas/dao/PeliculasDao.java
@@ -2,10 +2,10 @@ package com.svalero.peliculas.dao;
 
 import com.svalero.peliculas.model.Peliculas;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+
+import static com.svalero.peliculas.util.Constant.PAGE_SIZE;
 
 public class PeliculasDao {
     private Connection connection;
@@ -33,5 +33,63 @@ public class PeliculasDao {
         return affectedRows != 0;
     }
 
+    public int getCount(String search) throws SQLException {
+        String sql = "SELECT COUNT(*) FROM peliculas WHERE titulo LIKE ? OR sinopsis LIKE ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, "%" + search + "%");
+        statement.setString(2, "%" + search + "%");
+        ResultSet result = statement.executeQuery();
+        result.next();
+        int count = result.getInt(1);
+        statement.close();
+        return count;
+    }
 
+    public ArrayList<Peliculas> getAll(int page) throws SQLException {
+        String sql = "SELECT * FROM peliculas ORDER BY titulo LIMIT ?, ?";
+        return launchQuery(sql, page);
+    }
+
+    public ArrayList<Peliculas> getAll(int page, String search) throws SQLException {
+        if (search == null || search.isEmpty()) {
+            return getAll(page);
+        }
+
+        String sql = "SELECT * FROM peliculas WHERE titulo LIKE ? OR sinopsis LIKE ? ORDER BY titulo LIMIT ?, ?";
+        return launchQuery(sql, page, search);
+    }
+
+    private ArrayList<Peliculas> launchQuery(String query, int page, String... search) throws SQLException {
+        PreparedStatement statement;
+        ResultSet result;
+
+        statement = connection.prepareStatement(query);
+        if (search.length > 0) {
+            statement.setString(1, "%" + search[0] + "%");
+            statement.setString(2, "%" + search[0] + "%");
+            statement.setInt(3, page * PAGE_SIZE);
+            statement.setInt(4, PAGE_SIZE);
+        } else {
+            statement.setInt(1, page * PAGE_SIZE);
+            statement.setInt(2, PAGE_SIZE);
+        }
+
+        result = statement.executeQuery();
+        ArrayList<Peliculas> peliculaList = new ArrayList<>();
+        while (result.next()) {
+            Peliculas pelicula = new Peliculas();
+            pelicula.setIdPelicula(result.getInt("id_pelicula"));
+            pelicula.setTitulo(result.getString("titulo"));
+            pelicula.setSinopsis(result.getString("sinopsis"));
+            pelicula.setDuracion(result.getInt("duracion"));
+            pelicula.setFechaEstreno(result.getDate("fecha_estreno"));
+            pelicula.setDisponibleStreaming(result.getBoolean("disponible_streaming"));
+            pelicula.setImagen(result.getString("imagen"));
+            pelicula.setIdDirector(result.getInt("id_director"));
+            peliculaList.add(pelicula);
+        }
+
+        statement.close();
+        return peliculaList;
+    }
 }
---
+++ src/main/java/com/svalero/peliculas/util/Constant.java
@@ -0,0 +1,5 @@
+package com.svalero.peliculas.util;
+
+public class Constant {
+    public static final int PAGE_SIZE = 6;
+}
