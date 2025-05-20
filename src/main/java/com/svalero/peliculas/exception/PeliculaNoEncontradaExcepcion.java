0549a12521d581d67ddc6edfae691e481310b17f
Andrea Fernandez de la Rosa <a28707@svalero.com>
2025-05-20 20:29:38 +0200

    Creo la clase excepcion para pelicula

---
+++ src/main/java/com/svalero/peliculas/exception/PeliculaNoEncontradaExcepcion.java
@@ -0,0 +1,7 @@
+package com.svalero.peliculas.exception;
+
+public class PeliculaNoEncontradaExcepcion extends Exception{
+    public PeliculaNoEncontradaExcepcion() {
+        super("Película no encontrada");
+    }
+}
