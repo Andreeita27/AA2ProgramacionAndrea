973ba43449efa24340785499ba3e64e75c2d3368
Andrea Fernandez de la Rosa <a28707@svalero.com>
2025-05-21 21:38:31 +0200

    Añado la excepcion para los usuarios

---
+++ src/main/java/com/svalero/peliculas/exception/UsuarioNoEncontradoExcepcion.java
@@ -0,0 +1,8 @@
+package com.svalero.peliculas.exception;
+
+public class UsuarioNoEncontradoExcepcion extends Exception {
+
+    public UsuarioNoEncontradoExcepcion() {
+        super("Usuario no encontrado");
+    }
+}
