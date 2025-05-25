b9c013be438ed81c5a15e60d0d6f5f645ef47072
Andrea Fernandez de la Rosa <a28707@svalero.com>
2025-05-25 19:22:55 +0200

    Creo la excepcion de actores

---
+++ src/main/java/com/svalero/peliculas/exception/ActorNoEncontradoExcepcion.java
@@ -0,0 +1,7 @@
+package com.svalero.peliculas.exception;
+
+public class ActorNoEncontradoExcepcion extends Exception {
+    public ActorNoEncontradoExcepcion() {
+        super("Actor no encontrado");
+    }
+}
