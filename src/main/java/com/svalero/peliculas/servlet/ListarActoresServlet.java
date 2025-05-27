3cb8cc20e145280fe8d17a5f140641a1e0702274
Andrea Fernandez de la Rosa <a28707@svalero.com>
2025-05-27 17:54:50 +0200

    Implemento coherencia en el logeo de usuario

--- src/main/java/com/svalero/peliculas/servlet/ListarActoresServlet.java
+++ src/main/java/com/svalero/peliculas/servlet/ListarActoresServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
@@ -18,6 +19,12 @@ public class ListarActoresServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("usuario") == null) {
+            response.sendRedirect("login.jsp");
+            return;
+        }
 
         response.setCharacterEncoding("UTF-8");
 
